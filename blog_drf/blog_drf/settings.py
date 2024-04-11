0d2d83a6403a6cf9a9573578b09a507c269a85b8
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-04-11 22:15:40 +0300

    4

--- blog_drf/blog_drf/settings.py
+++ blog_drf/blog_drf/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'women.apps.WomenConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'women.apps.WomenConfig',
 ]
 
 MIDDLEWARE = [
--- blog_drf/women/views.py
+++ blog_drf/women/views.py
@@ -1,3 +1,20 @@
+from rest_framework import generics
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Women
+from .serializers import WomenSerializer
 
-# Create your views here.
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
+# simple realization of API request without serializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({"posts": list(lst)})
+
+    def post(self, request, *args, **kwargs):
+        return Response({"title": "Jehnifer"})
