0224649d657ec88111cd2827db76dfcb0b9103a4
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-04-15 13:57:39 +0300

    f_5_

--- blog_drf/women/serializers.py
+++ blog_drf/women/serializers.py
@@ -5,11 +5,10 @@ from rest_framework.renderers import JSONRenderer
 from .models import Women
 
 
-# class WomenModel:
-#     def __init__(self, title, content):
-#         self.title = title
-#         self.content = content
-
+# class WomenSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Women
+#         fields = '__all__'  # You can also explicitly specify the fields you want to include if needed
 
 class WomenSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
@@ -19,6 +18,26 @@ class WomenSerializer(serializers.Serializer):
     is_published = serializers.BooleanField(default=True)
     cat_id = serializers.IntegerField()
 
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.time_update = validated_data.get('time_update', instance.time_update)
+        instance.is_published = validated_data.get('is_published', instance.is_published)
+        instance.cat_id = validated_data.get('cat_id', instance.cat_id)
+        instance.save()
+        return instance
+
+
+
+
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
 
 # def encode():
 #     model = WomenModel('Women_1', 'Content_1')
@@ -26,8 +45,8 @@ class WomenSerializer(serializers.Serializer):
 #     print(model_sr.data, type(model_sr.data), sep='\n')
 #     json = JSONRenderer().render(model_sr.data)
 #     print(json)
-#
-#
+
+
 # def decode():
 #     stream = io.BytesIO(b'{"title":"Women_1","content":"Content_1"}')
 #     data = JSONParser().parse(stream)
--- blog_drf/women/views.py
+++ blog_drf/women/views.py
@@ -15,14 +15,36 @@ class WomenAPIView(APIView):
         w = Women.objects.all()
         return Response({"posts": WomenSerializer(w, many=True).data})
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id'],
-        )
+        return Response({"post": serializer.data})
 
-        return Response({"post": WomenSerializer(post_new).data})
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Object does not exists.'})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({"post": serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+
+        if not pk:
+            return Response({'error': 'Method DELETE not allowed.'})
+
+        Women.objects.filter(id=pk).delete()
+
+        return Response({'post': f'Post № {str(pk)} deleted'})
