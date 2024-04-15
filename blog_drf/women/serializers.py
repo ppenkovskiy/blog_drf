7f92be49cbad29af8550a0cbea30f67dd68283da
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-04-15 12:17:09 +0300

    f_5

--- blog_drf/women/serializers.py
+++ blog_drf/women/serializers.py
@@ -1,8 +1,36 @@
+import io
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+# class WomenModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField()
+    time_update = serializers.DateTimeField()
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = WomenModel('Women_1', 'Content_1')
+#     model_sr = WomenSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Women_1","content":"Content_1"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data) # dict
--- blog_drf/women/views.py
+++ blog_drf/women/views.py
@@ -1,20 +1,28 @@
+from django.forms import model_to_dict
 from rest_framework import generics
+from rest_framework import serializers
 from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Women
 from .serializers import WomenSerializer
 
 
-# class WomenAPIView(generics.ListAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
-
-# simple realization of API request without serializer
+# Simple realization of API request without serializer
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values()
-        return Response({"posts": list(lst)})
+        w = Women.objects.all()
+        return Response({"posts": WomenSerializer(w, many=True).data})
 
     def post(self, request, *args, **kwargs):
-        return Response({"title": "Jehnifer"})
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id'],
+        )
+
+        return Response({"post": WomenSerializer(post_new).data})
