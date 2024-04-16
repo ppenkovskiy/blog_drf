dc18392e030c835abc0ddecab0cb9ee945c10426
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2024-04-16 20:59:38 +0300

    f_11

--- blog_drf/blog_drf/urls.py
+++ blog_drf/blog_drf/urls.py
@@ -26,6 +26,7 @@ from women.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/women/', WomenAPIList.as_view()),
     path('api/v1/women/<int:pk>/', WomenAPIUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenAPIUpdate.as_view()),
